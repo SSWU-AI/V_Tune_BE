849fd975b26a5bb0097a1394f0ae363990b8cc5c
s-duswn <tlsduswnim@gmail.com>
2025-07-30 23:59:48 +0900

    fix: ASGI 설정에서 잘못된 settings 모듈 경로 수정

--- V_Tune/asgi.py
+++ V_Tune/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'v_tune_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'V_Tune_project.settings')
 
 application = get_asgi_application()
