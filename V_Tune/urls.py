e6bb4eca29d419d3f69538321b9d333b0e80ef63
s-duswn <tlsduswnim@gmail.com>
2025-07-30 23:28:27 +0900

    init: Django 프로젝트 초기 세팅

---
+++ V_Tune/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+def home(request):
+    return HttpResponse("성공적으로 배포되었습니다")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
