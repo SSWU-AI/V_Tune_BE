d8cf633912a67f542a0f6f792caa873a372649bc
s-duswn <tlsduswnim@gmail.com>
2025-08-05 17:27:00 +0900

    feat: Swagger 문서(swagger/redoc) URL 추가 및 불필요한 home 뷰 제거

--- V_Tune/urls.py
+++ V_Tune/urls.py
@@ -1,22 +1,28 @@
-# V_Tune/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.conf import settings
-from django.conf.urls.static import static  
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-def home(request):
-    return HttpResponse("성공적으로 배포되었습니다")
+schema_view = get_schema_view(
+   openapi.Info(
+      title="V-Tune API",
+      default_version='v1',
+      description="V-Tune API 문서입니다.",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/data/', include('data.urls')),  # 너가 만든 data urls 연결
     path('api/feedback/', include('feedback.urls')),
-    path('api/tts/', include('tts.urls')),
-    path('api/data/', include('data.urls')),
     path('api/compare/', include('compare.urls')),
-    path('', home),
-]
+    path('api/tts/', include('tts.urls')),
 
-# 개발 서버에서 정적 파일 서빙
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # ✅ Swagger 문서 URL 추가
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+]
