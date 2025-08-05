509b9d8e3d4c15b4a293af735b7ce0a012e1af16
s-duswn <tlsduswnim@gmail.com>
2025-08-05 15:28:12 +0900

    feat: feedback API 엔드포인트 및 include 경로 추가

--- V_Tune/urls.py
+++ V_Tune/urls.py
@@ -1,16 +1,17 @@
+# V_Tune/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
 
 def home(request):
     return HttpResponse("성공적으로 배포되었습니다")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tts/', include('tts.urls')),
+    path('api/feedback/', include('feedback.urls')),  # 최종 URL: /api/feedback/
+    path('api/tts/', include('tts.urls')),          
     path('', home),
 ]
 
