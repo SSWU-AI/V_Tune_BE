b66e29492570f639f917cc8cd9481f84f43671d1
s-duswn <tlsduswnim@gmail.com>
2025-08-05 18:31:07 +0900

    chore: 특정 도메인 주소 허용 제거

--- V_Tune/settings.py
+++ V_Tune/settings.py
@@ -149,7 +149,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://v-tune-iota.vercel.app",        # Vercel 배포 프론트
-    "https://v-tune-fe.onrender.com",        # Render 배포 프론트 (너가 FE 여기에 올렸다면)
-]
