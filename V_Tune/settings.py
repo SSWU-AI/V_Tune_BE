ae4eed93410abc06f4223bf3557c375ae5ae8cf7
s-duswn <tlsduswnim@gmail.com>
2025-08-05 18:09:46 +0900

    chore: CORS 허용 도메인 및 미들웨어 설정 추가

--- V_Tune/settings.py
+++ V_Tune/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
 
 
     'tts',
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -144,3 +146,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://v-tune-iota.vercel.app",        # Vercel 배포 프론트
+    "https://v-tune-fe.onrender.com",        # Render 배포 프론트 (너가 FE 여기에 올렸다면)
+]
