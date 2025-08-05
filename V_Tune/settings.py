c138b11e9242c4493bfe09a911b69f7bb7215779
s-duswn <tlsduswnim@gmail.com>
2025-08-05 18:55:09 +0900

    chore: 개발 중 CORS 모든 origin 허용 설정

--- V_Tune/settings.py
+++ V_Tune/settings.py
@@ -147,9 +147,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "https://v-tune-iota.vercel.app",  # 프론트 주소 정확히 명시
-]
+
 
