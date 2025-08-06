a68c4d76f6f1cb5d18bbc54a41a2ec48970a8dbf
s-duswn <tlsduswnim@gmail.com>
2025-08-06 16:24:19 +0900

    feat: Swagger 문서 경로 추가 및 기본 루트 경로에 텍스트 응답 설정

--- V_Tune/urls.py
+++ V_Tune/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import HttpResponse 
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -15,14 +16,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # 기본 경로에서 텍스트 응답
+    path('', lambda request: HttpResponse("배포가 완료되었습니다. /swagger/ 에서 API 문서를 확인하세요.")),
+
     path('admin/', admin.site.urls),
-    path('api/data/', include('data.urls')),  # 너가 만든 data urls 연결
+    path('api/data/', include('data.urls')), 
     path('api/feedback/', include('feedback.urls')),
     path('api/compare/', include('compare.urls')),
     path('api/tts/', include('tts.urls')),
 
-    # ✅ Swagger 문서 URL 추가
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
 ]
