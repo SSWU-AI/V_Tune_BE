f117703a730fb07a801f021c8317c7c885869b27
s-duswn <tlsduswnim@gmail.com>
2025-08-10 18:23:26 +0900

    refactor: compare_and_feedback로 뷰 함수 변경 및 URL 매핑 수정

--- compare/urls.py
+++ compare/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import compare_pose
+from .views import compare_and_feedback
 
 urlpatterns = [
-    path('', compare_pose, name='compare-pose'),
+    path('', compare_and_feedback, name='compare-and-feedback'),
 ]
